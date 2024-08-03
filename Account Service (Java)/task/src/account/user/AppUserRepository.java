3489025f2b25d765aff6647764913c8bbdc44939
Krste Vrkic <krste.vrkic@gmail.com>
2024-08-03 14:43:41 +0200

    removed files from version control

--- Account Service (Java)/task/src/account/user/AppUserRepository.java
+++
@@ -1,9 +0,0 @@
-package account.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface AppUserRepository extends CrudRepository<AppUser, Long> {
-    Optional<AppUser> findByUsername(String username);
-}
